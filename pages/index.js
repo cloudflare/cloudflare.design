c70a4e68b07ac21a28e44cf662aecdf8644060b3
mrmrs <hi@mrmrs.cc>
2018-12-14 19:42:21 +0000

    Made title responsive

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ const Home = props => (
   <Flex height='100vh' alignItems='center'>
     <Container py={[5, 6]} px={[3, 4, 5]}>
       <Div textAlign="center">
-        <H1 fontSize={7} mt={0} mb={2}>
+        <H1 fontSize={[4,5,7]} mt={0} mb={2}>
           Cloudflare Design
         </H1>
         <A
           color="gray.3"
           fontWeight={600}
-          fontSize={5}
+          fontSize={[3,5]}
           href="https://www.cloudflare.com/careers/departments/design/"
         >
           Come work with us
