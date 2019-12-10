b66cacab54a9f78c52156fbdeb5f879ee7759292
mrmrs <hi@mrmrs.cc>
2019-12-10 18:19:40 -0500

    Merge content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import jsx from "./jsx";
 import { ThemeProvider } from "theme-ui";
 import theme from "./theme";
+import data from './data'
 import { ConfigProvider } from "./useConfig";
 import NewConfigNotification from "./NewConfigNotification";
+import SiteTitle from './components/SiteTitle'
 
 // This is the current config from the worker
 // and just maps a key of our choosing to a value from our theme file
@@ -20,6 +22,7 @@ function App() {
     <ConfigProvider initialConfig={initialConfig}>
       {config => (
         <ThemeProvider theme={{ ...theme, c: { ...config } }}>
+      <div  sx={{ fontFamily: 'system-ui, sans-serif' }}>
           <header
             sx={{
               // We can then assign those values justt like we would a normal theme value and it gets picked up by theme-ui and converted into a value
@@ -29,11 +32,21 @@ function App() {
             }}
           >
             <NewConfigNotification />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a href="https://reactjs.org">Learn React</a>
-          </header>
+            <SiteTitle text={data.title} />
+        </header>
+        <section>
+          <h3 sx={{
+            fontSize: [3,5,6], 
+            textAlign: 'center' 
+          }}>
+            Locations
+          </h3>
+        </section>
+        <footer sx={{p: 2, borderTop: '1px solid'}}>
+          <small sx={{ fontSize: 0 }}>{data.copyright}</small>
+        </footer>
+      </div>
+
         </ThemeProvider>
       )}
     </ConfigProvider>
