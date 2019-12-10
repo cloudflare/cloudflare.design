9c26ced15ae4a42f3af37659e5a23eb5e19fa381
mrmrs <hi@mrmrs.cc>
2019-12-10 18:13:34 -0500

    Stubbing out some content. Removes margin and padding on body.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { ThemeProvider, jsx } from "theme-ui";
 import logo from "./logo.svg";
+import data from "./data";
+import SiteTitle from "./components/SiteTitle";
 import NewTheme from "./NewTheme";
 
 window.__THEME__ = {
@@ -16,23 +18,28 @@ delete window.__THEME__;
 function App() {
   return (
     <ThemeProvider theme={themeObject}>
-      <header sx={{ color: "primary" }}>
-        <NewTheme />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div  sx={{ fontFamily: 'system-ui, sans-serif' }}>
+        <header sx={{ 
+            color: "primary" 
+          }}>
+          <NewTheme />
+          <SiteTitle text={data.title} />
+        </header>
+        <section>
+          <h3 sx={{
+            fontSize: [3,5,6], 
+            textAlign: 'center' 
+          }}>
+            Locations
+          </h3>
+        </section>
+        <footer sx={{p: 2, borderTop: '1px solid'}}>
+          <small sx={{ fontSize: 0 }}>{data.copyright}</small>
+        </footer>
+      </div>
     </ThemeProvider>
   );
 }
 
 export default App;
+
--- src/data.js
+++ src/data.js
@@ -1,6 +1,37 @@
 const data = {
   title: 'Cloudflare Design',
   copyright: '© Copyright 2019',
+  locations: [
+    {
+      city: 'Lisbon',
+      image: 'lisbon.jpg',
+    },
+    {
+      city: 'London',
+      image: 'london.jpg',
+    },
+    {
+      city: 'San Francisco',
+      image: 'sf.jpg',
+    },
+    {
+      city: 'Austin',
+      name: 'Austin',
+      image: 'austin.jpg',
+    },
+  ],
+  social: [
+    {
+      name: "GitHub",
+      username: "cloudflare",
+      url: "https://github.com/cloudflare",
+    },
+    {
+      name: "Twitter",
+      username: "@cloudflare",
+      url: "https://twitter.com/cloudflare",
+    },
+  ]
 }
 
 export default data
