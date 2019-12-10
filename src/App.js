c5e63a1b43ac520db1da2d61c2c7ce2d03b94b4d
Sam Mason <smason@cloudflare.com>
2019-12-10 16:20:19 +0000

    Use create react app and setup global var for config

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const configObject = window.__CONFIG__;
+
+delete window.__CONFIG__;
+
+console.log(configObject);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
